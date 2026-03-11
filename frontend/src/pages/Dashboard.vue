e13d1ee79558444bf922101f39202ebd978718d5
Maksims Gnedovs <166118392+23DP1MGned@users.noreply.github.com>
2026-03-11 19:39:30 +0000

    frontend and backend install

---
+++ frontend/src/pages/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+
+<v-container>
+
+<h1>Dashboard</h1>
+
+<v-btn color="primary">
+Test button
+</v-btn>
+
+</v-container>
+
+</template>
---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,34 @@
+<template>
+
+<v-container class="fill-height d-flex align-center justify-center">
+
+<v-card width="400">
+
+<v-card-title>
+Login
+</v-card-title>
+
+<v-card-text>
+
+<v-text-field label="Email"></v-text-field>
+
+<v-text-field
+label="Password"
+type="password"
+></v-text-field>
+
+</v-card-text>
+
+<v-card-actions>
+
+<v-btn color="primary" block>
+Sign in
+</v-btn>
+
+</v-card-actions>
+
+</v-card>
+
+</v-container>
+
+</template>
